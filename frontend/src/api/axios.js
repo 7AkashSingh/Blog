5acc2a800e54dd773408e29492f335c5cad889e1
7AkashSingh <7akashsinghjma@gmail.com>
2026-07-24 01:52:20 +0530

    changing the axios path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://blog-production-983f.up.railway.app/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
