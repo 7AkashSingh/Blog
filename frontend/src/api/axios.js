f13ac847f7b71188f9471578813ecdb84097ec05
7AkashSingh <7akashsinghjma@gmail.com>
2026-07-24 13:16:47 +0530

    again changes to deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://blog-production-9ca1.up.railway.app/api/v1",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
