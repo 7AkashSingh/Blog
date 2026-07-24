838108e9d2ef22acf4df65411b198b305ff5e4c3
7AkashSingh <7akashsinghjma@gmail.com>
2026-07-24 12:53:16 +0530

    just again doing some connection issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://blog-production-9ca1.up.railway.app",
+    baseURL: "https://blog-production-9ca1.up.railway.app/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
